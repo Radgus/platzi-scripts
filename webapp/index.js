5400bdea4624f59e81a9af099bb2ec75829e88fc
LuisD <dominguezradgus@gmail.com>
2021-09-29 18:52:22 -0500

    coment when conditional in Jenkinsfile 10

--- webapp/index.js
+++ webapp/index.js
@@ -4,9 +4,20 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 const sum = require('./sum');
+const rest = require('./rest');
+
+router.get('/', () => {
+  const result = 'recuerda usar las rutas /add/number1/number2 y /rest/number1/number2';
+  return ctx.body = { result };
+});
 
 router.get('/add/:a/:b', (ctx, next) => {
   const result = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
+  return ctx.body = { result };
+});
+
+router.get('/rest/:a/:b', (ctx, next) => {
+  const result = rest(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
   return ctx.body = { result };
 });
 
